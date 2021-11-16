9f7ba9315edcc3dddf7f5b5a7bdd1f1555eeaa43
Caro <CA252755@fi365.ort.edu.uy>
2021-11-16 18:59:56 -0300

    ECS funcionando

--- ecs.tf
+++ ecs.tf
@@ -8,10 +8,72 @@ resource "aws_ecs_cluster" "cluster_appweb" {
   }
 }
 
-resource "aws_ecs_service" "ecs_appweb" {
-  name            = "ecs_appweb"
-  task_definition = aws_ecs_task_definition.service.arn
+resource "aws_ecs_task_definition" "task1" {
+  family                   = "task1"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  container_definitions = jsonencode([
+    {
+      name  = "first"
+      image = "caroarbiza/ecom:v1"
+
+
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+
+  ])
+}
+
+resource "aws_ecs_task_definition" "task2" {
+  family                   = "task2"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  container_definitions = jsonencode([
+    {
+      name = "second"
+
+
+      image     = "caroarbiza/ecom:v1"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+
+
+      ]
+    }
+  ])
+
+
+  # volume {
+  #   name      = "service-storage"
+  #   host_path = "/ecs/service-storage"
+  # }
+}
+
+
+resource "aws_ecs_service" "ecs_task1" {
+  name            = "ecs_task1"
+  task_definition = aws_ecs_task_definition.task1.arn
   cluster         = aws_ecs_cluster.cluster_appweb.id
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets = [aws_subnet.subnet_public_one.id, aws_subnet.subnet_public_two.id]
+    security_groups = [aws_security_group.allow_ssh_http.id]
+  }
 
   load_balancer {
     target_group_arn = aws_lb_target_group.appwbeb_tg.arn
@@ -21,44 +83,26 @@ resource "aws_ecs_service" "ecs_appweb" {
 
 }
 
-resource "aws_ecs_task_definition" "service" {
-  family = "service"
-  container_definitions = jsonencode([
-    {
-      name      = "first"
-      image     = "caroarbiza/ecom:v1"
-      cpu       = 1
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 8080
-        }
-      ]
-    },
-    {
-      name      = "second"
-      cpu       = 1
-      memory    = 512
-      image     = "caroarbiza/ecom:v1"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 8081
-        }
-      ]
-    }
-  ])
-
-  # volume {
-  #   name      = "service-storage"
-  #   host_path = "/ecs/service-storage"
-  # }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-1a, us-west-1e]"
+resource "aws_ecs_service" "ecs_task2" {
+  name            = "ecs_task2"
+  task_definition = aws_ecs_task_definition.task2.arn
+  cluster         = aws_ecs_cluster.cluster_appweb.id
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets = [aws_subnet.subnet_public_one.id, aws_subnet.subnet_public_two.id]
+    security_groups = [aws_security_group.allow_ssh_http.id]
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.appwbeb_tg.arn
+    container_name   = "second"
+    container_port   = 80
+  }
+
 }
+
+
+
+
+
+
